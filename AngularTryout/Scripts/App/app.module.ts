1a27001c61eadd62363ab8c81640a8f895219ee2
Roman Kostyk <roman.kostyk@actum.cz>
2017-06-20 15:00:35 +0200

    Added home component, moved services.

--- AngularTryout/Scripts/App/app.module.ts
+++ AngularTryout/Scripts/App/app.module.ts
@@ -1,13 +1,22 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+//import { RouterModule } from '@angular/router'
+
 import { HelloWorldComponent } from './components/hello-world/app.component.hello-world';
 import { LoginComponent } from './components/login/app.component.login';
+import { HomeComponent } from './components/home/app.component.home';
+
+const routes = [
+    //{ path: '', component: LoginComponent, useAsDefault: true, outlet: 'login' },
+    //{ path: 'login', component: LoginComponent, outlet: 'login'},
+    //{ path: 'home', component: HomeComponent }
+];
 
 @NgModule({
-    imports: [BrowserModule, HttpModule],
-    declarations: [HelloWorldComponent, LoginComponent],
-    bootstrap: [HelloWorldComponent, LoginComponent]
+    imports: [BrowserModule, HttpModule], // RouterModule.forRoot(routes)],
+    declarations: [HelloWorldComponent, LoginComponent, HomeComponent],
+    bootstrap: [HelloWorldComponent, LoginComponent, HomeComponent]
 })
 
 export class AppModule { }
---
+++ AngularTryout/Scripts/App/components/home/app.component.home.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'home',
+    templateUrl: './Scripts/App/components/home/app.component.home.html'
+})
+
+export class HomeComponent { }
